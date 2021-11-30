91901e5cc05aa698e12b5794620bff2a84240a75
Jake Dustin <jakedustin097@gmail.com>
2021-11-30 09:29:37 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Wind River Web Client',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
