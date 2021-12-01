11e9381e0971ced2a8434c8e05f70449fcf207e2
mccannmann <mccannmann1@gmail.com>
2021-11-30 21:38:54 -0700

    Add edit/add/delete question functions

--- src/app/survey-builder/survey-builder.page.ts
+++ src/app/survey-builder/survey-builder.page.ts
@@ -5,11 +5,37 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './survey-builder.page.html',
   styleUrls: ['./survey-builder.page.scss'],
 })
-export class SurveyBuilderPage implements OnInit {
+export class SurveyBuilderPage {
 
+  questions = new Array();
+  numQuestions = 0;
   constructor() { }
 
-  ngOnInit() {
+  toggleEdit(question) {
+    // const index = this.questions.indexOf(question.index);
+    // if (this.questions.length > 0) {
+    //   this.questions[index].isEditing = !this.questions[index].isEditing;
+    // }
+    question.isEditing = !question.isEditing;
+  }
+
+  addQuestion() {
+    this.questions.push(
+        {
+          index: this.numQuestions,
+          isEditing: false
+        }
+      );
+    ++this.numQuestions;
+  }
+
+  removeQuestion(question) {
+    const index = this.questions.indexOf(question.index);
+    if (index > -1) {
+      this.questions.splice(index, 1);
+    } else {
+      this.questions.pop();
+    }
   }
 
 }
