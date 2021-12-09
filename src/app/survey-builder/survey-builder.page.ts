f68f3b3b6c23cc66d491713cbaf6a7e6a37cc539
mccannmann <mccannmann1@gmail.com>
2021-12-08 19:41:28 -0700

    Add customization options for questions

--- src/app/survey-builder/survey-builder.page.ts
+++ src/app/survey-builder/survey-builder.page.ts
@@ -14,7 +14,8 @@ export class SurveyBuilderPage {
     dueDate: 'tomorrow',
     questions: [],
   };
-  constructor(private api: SurveyBuilderApi) { }
+
+  constructor(private api: SurveyBuilderApi) {  }
 
   toggleEdit(question) {
     question.isEditing = !question.isEditing;
@@ -25,17 +26,32 @@ export class SurveyBuilderPage {
         {
           textHtml: '',
           type: '',
-          isEditing: false
+          isEditing: false,
+          options: []
         }
       );
   }
 
   removeQuestion(question) {
-    const index = this.survey.questions.indexOf(question.index);
+    const index = this.survey.questions.indexOf(question);
     if (index > -1) {
       this.survey.questions.splice(index, 1);
     } else {
       this.survey.questions.pop();
+    }
+  }
+
+  addOption(question) {
+    question.options.push({
+      label: '',
+      value: ''
+    })
+  }
+
+  removeOption(question, option) {
+    const index = question.options.indexOf(option);
+    if (index > -1) {
+      question.options.splice(index, 1);
     }
   }
 
@@ -48,7 +64,7 @@ export class SurveyBuilderPage {
       case 'BOOLEAN':
         break;
       case 'CHECK':
-        question.options = [{label: '', value: ''}, {label: '', value: ''}, {label: '', value: ''}, {label: '', value: ''}];
+        question.options = [{label: '', value: ''}];
         break;
       case 'SLIDER':
         question.startLabel = 'START';
@@ -56,14 +72,27 @@ export class SurveyBuilderPage {
         question.min = 1;
         question.max = 6;
         question.step = 1;
+
+        //If we want to make min/max number options dynamic
+        //question.minNumbers = Array(question.max - question.min).fill(null).map((x,i)=>i + 1);
+        //question.maxNumbers = Array(11 - question.min).fill(null).map((x,i)=>i + 1 + question.min);
         break;
       case 'RADIO':
-        question.options = [{label: '', value: ''}, {label: '', value: ''}, {label: '', value: ''}, {label: '', value: ''}];
+        question.options = [{label: '', value: ''}];
         break;
       default:
         break;
     }
   }
+
+  // onStepSelect() {
+  //   var element = document.getElementById(id);
+  //   console.log(id);
+  //   question.min = 1;
+  //   question.max = element;
+  //   console.log(question.min);
+  //   console.log(question.max);
+  // }
 
   saveSurvey() {
     // TODO: format questions to remove excess fields
