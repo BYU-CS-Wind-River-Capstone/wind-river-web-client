b7f9b244f9da05e6dc5d0f532cd47950ec70af0f
Nathan Craven <cravennathan@gmail.com>
2022-02-10 21:29:51 -0700

    removed merge conflict artifacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,15 +13,13 @@ const routes: Routes = [
     loadChildren: () => import('./survey-builder/survey-builder.module').then( m => m.SurveyBuilderPageModule)
   },
   {
-<<<<<<< HEAD
     path: 'data-management',
     loadChildren: () => import('./data-management/data-management.module').then(m => m.DataManagementPageModule)
-  }
-=======
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
->>>>>>> master
 ];
 
 @NgModule({
