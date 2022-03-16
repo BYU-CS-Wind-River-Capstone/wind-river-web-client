0fc5af496a890ab90d87560981c9ed004a355672
Jake Dustin <jakedustin097@gmail.com>
2022-03-16 01:38:05 -0600

    added variables for user name and password

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,12 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-import { RegisterPageRoutingModule } from './register-routing.module';
-
+import { NgModule } from '@angular/core';
 import { RegisterPage } from './register.page';
+import { RegisterPageRoutingModule } from './register-routing.module';
 
 @NgModule({
   imports: [
